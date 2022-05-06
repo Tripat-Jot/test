5b8181b212bd989a022aeb6a02e2adf2a747fe79
Tripat-Jot <triptjot3@gmail.com>
2022-05-06 20:39:27 +0530

    Changes to Index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.json({message: 'Welcome to App'});
-  res.send('<p>Welcome to Azure <br> by Tripatjot Singh</p> <p><a href="/quotes"> Data Extracted from Stored Procedure </a></p>') ;
+  res.send('<p><h2>Welcome to Azure by Tripatjot Singh </h2></p> <p><a href="/quotes"> Data Extracted from Stored Procedure </a></p>') ;
 });
 
 module.exports = router;
