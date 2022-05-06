03506cc502425e838787d7d8166da410cf6eaaa6
Tripat-Jot <triptjot3@gmail.com>
2022-05-06 20:09:30 +0530

    changes

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.json({message: 'Welcome to App'});
-  res.send('<p>Welcome to Azure by Tripatjot Singh</p>') ;
+  res.send('<p>Welcome to Azure <br> by Tripatjot Singh</p> <p><a href="/quotes"> Data Extracted from Stored Procedure </a></p>') ;
 });
 
 module.exports = router;
